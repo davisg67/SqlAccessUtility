546aa1288d150354fdaa3913a2978bc3adc06c21
Greg Davis <gdavis@davtechsolutions.com>
2019-02-26 22:22:14 -0500

    Initial commit

---
+++ DataAccessUtility/DataAccessUtility/SqlDatabaseUtility.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessUtility
+{
+    public class SqlDatabaseUtility
+    {
+        //Opens the database connection.
+        public SqlConnection GetConnection(string connectionName, ref string sqlConnError)
+        {
+            sqlConnError = string.Empty;
+            string cnstr = ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+            SqlConnection cn = new SqlConnection(cnstr);
+
+            try
+            {
+                cn.Open();
+                return cn;
+            }
+            catch (SqlException ex)
+            {
+                sqlConnError = "GetConnection() error: " + ex.ToString();
+                return cn;  
+            }
+            
+        }
+
+        //Executes a stored procedure that performs a query
+        public DataSet ExecuteQuery(string connectionName, string storedProcName, Dictionary<string, SqlParameter> procParameters, ref string sqlQueryError)
+        {
+            string sqlConnError = string.Empty;
+            DataSet ds = new DataSet();
+
+            try
+            {
+                using (SqlConnection cn = GetConnection(connectionName, ref sqlConnError))
+                {
+                    sqlQueryError = sqlConnError;
+                    using (SqlCommand cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = storedProcName;
+                        // assign parameters passed in to the command
+                        foreach (var procParameter in procParameters)
+                        {
+                            cmd.Parameters.Add(procParameter.Value);
+                        }
+
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            da.Fill(ds);
+                        }
+                    }
+                }
+                return ds;
+            }
+            catch(SqlException ex)
+            {
+                if (sqlQueryError == string.Empty)
+                {
+                    sqlQueryError = "ExecuteQuery() error: " + ex.ToString();
+                }
+                else
+                {
+                    sqlQueryError += "   ExecuteQuery() error: " + ex.ToString();
+                }
+               
+                return ds;
+            }
+            
+        }
+
+        //Executes a stored procedure that performs an insert, update or delete.
+        public int ExecuteCommand(string connectionName, string storedProcName, Dictionary<string, SqlParameter> procParameters, ref string sqlCmdError)
+        {
+            int rc = -1;
+            string sqlConnError = string.Empty;
+
+            try
+            {
+                using (SqlConnection cn = GetConnection(connectionName, ref sqlConnError))
+                {
+                    sqlCmdError = sqlConnError;
+                    // create a SQL command to execute the stored procedure
+                    using (SqlCommand cmd = cn.CreateCommand())
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = storedProcName;
+                        // assign parameters passed in to the command
+                        foreach (var procParameter in procParameters)
+                        {
+                            cmd.Parameters.Add(procParameter.Value);
+                        }
+                        rc = cmd.ExecuteNonQuery();
+                    }
+                }
+                return rc;  //The number of rows inserted, update or deleted by the command.
+            }
+            catch(SqlException ex)
+            {
+                if (sqlCmdError == string.Empty)
+                {
+                    sqlCmdError = "ExecuteCommand() error: " + ex.ToString();
+                }
+                else
+                {
+                    sqlCmdError += "   ExecuteCommand() error: " + ex.ToString();
+                }
+
+                return rc;
+            }
+            
+        }
+
+
+    }
+}
---
+++ DataAccessUtilityConsole/DataAccessUtilityConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessUtility;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace DataAccessUtilityConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlError = string.Empty;
+            SqlDatabaseUtility dbutility = new SqlDatabaseUtility();
+
+            // add a customer
+            Dictionary<string, SqlParameter> cmdParameters = new Dictionary<string, SqlParameter>();
+            cmdParameters["name"] = new SqlParameter("name", "Smith");
+            cmdParameters["state"] = new SqlParameter("state", "MD");
+            dbutility.ExecuteCommand("mssqltips", "dbo.AddCustomer", cmdParameters, ref sqlError);
+
+            if (sqlError != string.Empty)
+            {
+                Console.WriteLine(sqlError);
+                Console.ReadLine();
+            }
+            else
+            {
+                //Get the customer list.
+                Dictionary<string, SqlParameter> queryParameters = new Dictionary<string, SqlParameter>();
+
+                //Passing in the connection name, stored procedure name, and the parameter collection.
+                DataSet ds = dbutility.ExecuteQuery("mssqltips", "dbo.GetCustomerList", queryParameters, ref sqlError);
+                DataTable t = ds.Tables[0];
+                foreach (DataRow r in t.Rows)
+                {
+                    Console.WriteLine(string.Format("{0}\t{1}\t{2}",
+                      r[0].ToString(),
+                      r[1].ToString(),
+                      r[2].ToString()));
+                }
+
+                if (sqlError != string.Empty)
+                    Console.WriteLine(sqlError);
+
+                Console.ReadLine(); //Hold the console window for user input (key press).
+            }
+
+            
+        }
+    }
+}
